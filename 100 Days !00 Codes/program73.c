ffb584f15e4b8836506b11a612c89daaf6eebefc
lokendrasharma-pix <lokendrasharma644@gmail.com>
2025-10-06 01:27:41 +0530

    Add files via upload

---
+++ 100 Days !00 Codes/program73.c
@@ -0,0 +1,15 @@
+// Q74: Find the transpose of a matrix.
+
+/*
+Sample Test Cases:
+Input 1:
+2 3
+1 2 3
+4 5 6
+Output 1:
+1 4
+2 5
+3 6
+
+*/
+
---
+++ 100 Days !00 Codes/program76.c
@@ -0,0 +1,21 @@
+// Q77: Check if the elements on the diagonal of a matrix are distinct.
+
+/*
+Sample Test Cases:
+Input 1:
+3 3
+1 2 3
+4 5 6
+7 8 1
+Output 1:
+False
+
+Input 2:
+3 3
+1 2 3
+4 5 6
+7 8 9
+Output 2:
+True
+
+*/
